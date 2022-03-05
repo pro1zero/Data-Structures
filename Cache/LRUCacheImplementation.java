edb2ca61a4b166fcee18d5bfb139748c80cb02b4
Jenish Soni <39327512+pro1zero@users.noreply.github.com>
2022-03-05 14:05:23 -0500

    ADDED both solutions for LRU Cache

---
+++ Cache/LRUCacheImplementation.java
@@ -0,0 +1,137 @@
+package assignment3;
+import java.util.*;
+public class LRUCacheImplementation {
+	
+	class LRUCache{
+		Map<String, Node> cache = new HashMap<>();
+		int maxSize;
+		int currentSize = 0;
+		LinkedList listOfMostRecent = new LinkedList();
+		
+		public LRUCache(int maxSize) {
+			this.maxSize = maxSize > 1 ? maxSize : 1;
+		}
+		
+		public void insertKeyValuePair(String key, int value) {
+			if(!cache.containsKey(key)) {
+				if(currentSize == maxSize) {
+					evictLeastUsed();
+				}
+				else {
+					currentSize += 1;
+				}
+				cache.put(key, new Node(key, value));
+			}
+			else {
+				if(!cache.containsKey(key)) {
+					return;
+				}
+				this.cache.get(key).value = value;
+			}
+			updateMostRecent(cache.get(key));
+			
+		}
+		
+		public LRUResult getValueFromKey(String key) {
+			if(!cache.containsKey(key)) {
+				return new LRUResult(false, -1);
+			}
+			updateMostRecent(cache.get(key));
+			return new LRUResult(true, cache.get(key).value);
+		}
+		
+		public void updateMostRecent(Node node) {
+			listOfMostRecent.setHeadTo(node);
+		}
+		
+		public String getMostRecent() {
+			if(listOfMostRecent.head == null) {
+				return "";
+			}
+			return listOfMostRecent.head.key;
+		}
+		
+		public void evictLeastUsed() {
+			String keyToRemove = listOfMostRecent.tail.key;
+			listOfMostRecent.removeTail();
+			cache.remove(keyToRemove);
+		}
+		
+		
+	}
+	
+	class LinkedList{
+		Node head = null;
+		Node tail = null;
+		
+		public void setHeadTo(Node node) {
+			if(head == node) {
+				return;
+			}
+			else if(head == null) {
+				head = node;
+				tail = node;
+			}
+			else if(head == tail) {
+				tail.prev = node;
+				head = node;
+				head.next = tail;
+			}
+			else {
+				if(tail == node) {
+					removeTail();
+				}
+				node.removeBindings();
+				head.prev = node;
+				node.next = head;
+				head = node;
+			}
+		}
+		
+		public void removeTail() {
+			if(tail == null) {
+				return;
+			}
+			if(tail == head) {
+				head = null;
+				tail = null;
+				return;
+			}
+			tail = tail.prev;
+			tail.next = null;
+		}
+	}
+	
+	class Node{
+		String key;
+		int value;
+		Node prev;
+		Node next;
+		
+		public Node(String key, int value) {
+			this.key = key;
+			this.value = value;
+		}
+		
+		public void removeBindings() {
+			if(prev != null) {
+				prev.next = next;
+			}
+			if(next != null) {
+				next.prev = prev;
+			}
+			prev = null;
+			next = null;
+		}
+	}
+	
+	class LRUResult{
+		boolean found;
+		int value;
+		
+		public LRUResult(boolean found, int value) {
+			this.found = found;
+			this.value = value;
+		}
+	}
+}
